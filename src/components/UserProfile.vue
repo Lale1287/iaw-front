955c7ea6e24196056f2d4a2bcf8c98186b45e98b
lale <alejandracolombini1287@gmail.com>
2023-10-26 18:52:52 -0300

    profile format

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,26 +1,34 @@
-<template>
-    <div>
-        <h1>Profile Page</h1>
-        <div>
-            <div>
-                <div>
-                    <img :src="user.picture"/>
-                    <div>
-                        <h2>{{ user.name }}</h2>
-                        <span>{{ user.email }}</span>
-                    </div>
-                </div>
-                <div>
-                    <span title="User Token">{{ accessToken }}</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script setup>
     import { userAuthStore } from '@/stores/userAuthStore.js'
     const authStore = userAuthStore()
     const user = authStore.getUserData
     const accessToken = authStore.getToken
 </script>
+
+<template>
+    <v-container>
+        <div class="recuadro-large">
+            <h1>Profile Page</h1>
+            <div>
+                <div>
+                    <div>
+                        <img :src="user.picture"/>
+                        <div>
+                            <h2>{{ user.name }}</h2>
+                            <span>{{ user.email }}</span>
+                        </div>
+                    </div>
+                    <div class="ustk">
+                        <span title="User Token">{{ accessToken }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </v-container>
+</template>
+
+<style>
+.ustk{
+    overflow: scroll;
+}
+</style>
